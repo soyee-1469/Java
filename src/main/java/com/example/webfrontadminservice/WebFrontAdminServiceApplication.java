b15a3bc2e4bc4c2997a4ece6d3cfc2347d37e144
soyee-1469 <soyee1469@gmail.com>
2024-11-25 10:57:34 +0900

    202411251057

---
+++ src/main/java/com/example/webfrontadminservice/WebFrontAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfrontadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrontAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFrontAdminServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfrontadminservice/WebFrontAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfrontadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFrontAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
